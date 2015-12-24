ca945af7952d038e31f09a20a5db5d085aeb286b
David Daniel <davedan7@gmail.com>
2015-12-24 11:33:29 -0500

    Allergies

--- javascript/allergies/allergies.js
+++ javascript/allergies/allergies.js
@@ -1,76 +1,29 @@
 'use strict';
-var _ = require('lodash');
-
-var allergies = {
-  128: 'cats',
-   64: 'pollen',
-   32: 'chocolate',
-   16: 'tomatoes',
-    8: 'strawberries',
-    4: 'shellfish',
-    2: 'peanuts',
-    1: 'eggs'
-};
+let _ = require('lodash');
 
 class Allergies {
-  constructor(number) {
-    this.number = number;
+  constructor(index) {
+    this.index = index;
+    this.possibleAllergies = [ 'eggs', 'peanuts', 'shellfish', 'strawberries', 'tomatoes', 'chocolate', 'pollen', 'cats' ];
   }
 
   list() {
-    let ret = [];
-    if (!this.number) { return ret; } // handle no allergies
-    if (allergies[this.number]) {
-      ret.push(allergies[this.number]);
-      return ret;
-    }
+    let ret          = [];
+    let allergyIndex = this.index;
 
-    for (var n in allergies) {
-
-    }
+    this.possibleAllergies.forEach(function (allergy, i) {
+      if (allergyIndex & Math.pow(2, i)) {
+        ret.push(allergy);
+      }
+    });
+    return ret;
   }
+
+  allergicTo(item) {
+    let list = this.list();
+    return _.includes(list, item);
+  }
+
 }
 
-
 module.exports = Allergies;
-
-//   var allergies = {
-//     128: 'cats',
-//      64: 'pollen',
-//      32: 'chocolate',
-//      16: 'tomatoes',
-//       8: 'strawberries',
-//       4: 'shellfish',
-//       2: 'peanuts',
-//       1: 'eggs'
-//   };
-//
-//   return {
-//     list: function() {
-//       var ret = [];
-//       ret.push(allergies[n]);
-//
-//       return n ? ret : [];
-//     }
-//   };
-//
-//
-// };
-  // return {
-  //   list: function () {
-  //     var ret   = [];
-  //     var adder = function (number) {
-  //       for (var num in allergies) {
-  //         if (number % num === 0 && number >= num) {
-  //           ret.push(allergies[num]);
-  //           adder(number - num);
-  //         } else if (number === 1) {
-  //           ret.push(allergies[1]);
-  //         }
-  //       }
-  //     };
-
-  //     adder(n);
-  //     return n ? _.uniq(ret) : [];
-  //   }
-  // };
--- javascript/allergies/allergies_test.spec.js
+++ javascript/allergies/allergies_test.spec.js
@@ -27,39 +27,39 @@ describe('Allergies',function() {
     expect(allergies.list()).toEqual([ 'eggs', 'peanuts' ]);
   });
 
-  xit("allergies to more than eggs but not peanuts", function() {
+  it("allergies to more than eggs but not peanuts", function() {
     var allergies = new Allergies(5);
     expect(allergies.list()).toEqual([ 'eggs', 'shellfish' ]);
   });
 
-  xit("allergic to lots of stuff", function() {
+  it("allergic to lots of stuff", function() {
     var allergies = new Allergies(248);
     expect(allergies.list()).toEqual(["strawberries", "tomatoes", "chocolate", "pollen", "cats"]);
   });
 
-  xit("allergic to everything", function() {
+  it("allergic to everything", function() {
     var allergies = new Allergies(255);
     expect(allergies.list()).toEqual(["eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"]);
   });
 
-  xit("no allergic means not allergic", function() {
+  it("no allergic means not allergic", function() {
     var allergies = new Allergies(0);
     expect(allergies.allergicTo('peanuts')).toEqual(false);
     expect(allergies.allergicTo('cats')).toEqual(false);
     expect(allergies.allergicTo('strawberries')).toEqual(false);
   });
 
-  xit("allergic to eggs", function() {
+  it("allergic to eggs", function() {
     var allergies = new Allergies(1);
     expect(allergies.allergicTo('eggs')).toEqual(true);
   });
 
-  xit("allergic to eggs and other things", function() {
+  it("allergic to eggs and other things", function() {
     var allergies = new Allergies(5);
     expect(allergies.allergicTo('eggs')).toEqual(true);
   });
 
-  xit("ignore non allergen score parts", function() {
+  it("ignore non allergen score parts", function() {
     var allergies = new Allergies(509);
     expect(allergies.list()).toEqual(["eggs", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"]);
   });
